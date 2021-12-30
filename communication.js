3337572c7bf7960e1d025e3be96e737fda3b008d
idoganzer <idoganzer@gmail.com>
2021-12-30 16:38:02 +0200

    Create communication.js

---
+++ communication.js
@@ -0,0 +1,32 @@
+/**
+ * Creates a request string for the api get request
+ * @param obj The base request string
+ * @param path The API path
+ * @return {string} A request string
+ */
+const createBaseString =
+    (obj, path) => obj.URL + path +  '?apikey=' + obj.key;
+
+/**
+ * Returns two dates separated by the range indicated by the range param
+ * @param range
+ * @return {[string, string]}
+ */
+const getDate = range => {
+    let now = new Date(),
+        startString = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + (now.getDate()),
+        end = new Date((new Date()).setDate(now.getDate() + (range || 7))),
+        endString = end.getFullYear() + "-" + (end.getMonth() + 1) + "-" + (end.getDate());
+    return [startString,endString]
+};
+
+/**
+ * getDataByType Takes and returns the results of fetching a specific API route
+ * @param type An API route
+ * @param range A parameter for the API call
+ * @return {Promise<any>}
+ */
+export const getDataByType = async (type, range) => {
+    const response = await fetch(pageDispatch[type](apiConf, range));
+    return [{[type] : await response.json()}]
+};
