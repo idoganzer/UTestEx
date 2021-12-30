1fea4f50d60b46d096f4e6bcc273db56779e8fb5
idoganzer <idoganzer@gmail.com>
2021-12-30 16:44:05 +0200

    Update communication.js

--- communication.js
+++ communication.js
@@ -14,7 +14,7 @@ const createBaseString =
  */
 const getDate = range => {
     let now = new Date(),
-        startString = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + (now.getDate()),
+        startString = now.getMonth() + "-" + (now.getFullYear() + 1) + "-" + (now.getDate()),
         end = new Date((new Date()).setDate(now.getDate() + (range || 7))),
         endString = end.getFullYear() + "-" + (end.getMonth() + 1) + "-" + (end.getDate());
     return [startString,endString]
